8e16585750802eca9b2b39a0c211f0d0c7b7525f
SharvariPawar10 <sharvarip49@gmail.com>
2024-06-22 23:14:04 +0530

    Add files via upload

---
+++ batsman.py
@@ -0,0 +1,5 @@
+def findaverage(runs,matches):
+    return runs / matches
+
+def strikerate(runs,balls):
+    return (runs / balls)*100
---
+++ bowler.py
@@ -0,0 +1,6 @@
+def findavg(runs,wickets):
+    return runs / wickets
+
+def economyrate(overs,runs):
+    return runs / overs
+    
